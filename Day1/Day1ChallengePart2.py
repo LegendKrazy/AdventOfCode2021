70894d856e7b048db2564a844d85b07aa1056849
Karl Daniti <kdaniti@gmail.com>
2021-12-05 13:09:20 -0500

    Advent of Code 2021 Challenges - Day 1 completed. Preparing for Day 2

--- Day1/Day1ChallengePart2.py
+++ Day1/Day1ChallengePart2.py
@@ -1,4 +1,4 @@
-# # Advent of Code 2021 Day 2 Challenge
+# # Advent of Code 2021 Day 1 part 2 Challenge
 # Your goal now is to count the number of times the sum of measurements in this sliding window
 # increases from the previous sum. So, compare A with B, then compare B with C, then C with D,
 # and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
@@ -6,7 +6,7 @@
 windows = []
 largerwindows = 0
 
-with open("Day2Input.txt") as file:
+with open("Day1Input.txt") as file:
     while line := file.readline().rstrip():
         windows.append(int(line))
 
---
+++ Day2/Day2Challenge.py
@@ -0,0 +1,10 @@
+# Advent of Code 2021 Day 2 Challenge
+# forward X increases the horizontal position by X units.
+# down X increases the depth by X units.
+# up X decreases the depth by X units.
+# Your horizontal position and depth both start at 0
+# Calculate the horizontal position and depth you would have after following the planned course.
+# What do you get if you multiply your final horizontal position by your final depth?
+
+depth = 0
+x_position = 0
